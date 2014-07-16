8091fa486d682c129bab6a86e7f4419157e46f76
David Schlossman <dschlossman@gmail.com>
2014-07-15 23:57:30 -0500

    updated initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,3 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 'paperclipdemo1337.s3.amazonaws.com'
